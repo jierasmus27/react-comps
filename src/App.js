b869dce4e406848c3d32053b44342c091f3b7c74
Jaco Erasmus <jaco.erasmus@platform45.com>
2026-03-08 19:30:05 +0200

    add: initial Button props

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Button from './Button';
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div>
+    <div>
+      <Button success rounded outline>Click me</Button>
+    </div>
+    <div>
+      <Button warning>Click me</Button>
+    </div>
+    <div>
+      <Button secondary rounded>Click me</Button>
+    </div>
+    <div>
+      <Button primary outline>Click me</Button>
+    </div>
+    <div>
+      <Button>Click me</Button>
+    </div>
+  </div>;
 }
 
 export default App;

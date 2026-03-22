97d40b8952cea9e256a3c28dbaa06c1a8cdf46ec
Jaco Erasmus <jierasmus27@gmail.com>
2026-03-22 13:08:12 +0200

    style: enhance DropDown component with improved styling and layout for better user experience

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -16,11 +16,14 @@ function DropDown({ options, value, onChange }) {
     return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>;
   });
 
-  console.log(dropDownOptions);
-
-  return <div>
-    <div onClick={handleOpenClick}>{value?.label || 'Select...'}</div>
-    {menuOpen && <div>{dropDownOptions}</div>}
+  return <div className="w-48 relative">
+    <div
+     className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+     onClick={handleOpenClick}
+     >
+      {value?.label || 'Select...'}
+    </div>
+    {menuOpen && <div className="absolute top-full left-0 w-full bg-white shadow rounded p-3">{dropDownOptions}</div>}
   </div>;
 }
 
